3a16fcddae3629fe0a224c9e035e640493b49aa8
Matheus Maschio <matheuslmaschi0@hotmail.com>
2023-09-06 14:37:41 -0300

    feat: beta-screens Sign in and Sign Up

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { 
+    Background, 
+    Container, 
+    AreaInput, 
+    Input, 
+    SubmitButton, 
+    SubmitText, 
+} from '../Styles/StyleSignUpScreen';
+
+import { useNavigation } from '@react-navigation/native';
+import { StackTypes } from '../routes/auth.routes';
 
 export default function SignInScreen() {
+    const navigation = useNavigation<StackTypes>();
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View>
-            <Text>SignInScreen</Text>
-        </View>
-    )
+        <Background>
+        <Container>
+
+            <AreaInput>
+                <Input
+                    placeholder="Seu email"
+                    value={email}
+                    onChangeText={ (text) => setEmail(text) }
+                />
+            </AreaInput>
+
+            <AreaInput>
+                <Input
+                    placeholder="Sua senha"
+                    value={password}
+                    onChangeText={ (text) => setPassword(text) }
+                    secureTextEntry={true}
+                />
+            </AreaInput>
+
+            <SubmitButton onPress={() => navigation.navigate('Home')}>
+                <SubmitText>Logar</SubmitText>  
+            </SubmitButton>
+            
+        </Container>
+        </Background>
+    );
 }
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,10 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { 
+  Background, 
+  Container, 
+  AreaInput, 
+  Input, 
+  SubmitButton, 
+  SubmitText, 
+} from '../Styles/StyleSignUpScreen';
+
+import firebase from '../dataBase/firebaseConnection';
+
+
+import { useNavigation } from '@react-navigation/native';
+import { StackTypes } from '../routes/auth.routes';
 
 export default function SignUpScreen() {
+  const navigation = useNavigation<StackTypes>();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // async function signUp(){
+  //   await firebase.auth().createUserWithEmailAndPassword(email, password);
+  // }
+
   return (
-    <View>
-      <Text>SignUpScreen</Text>
-    </View>
-  )
+    <Background>
+      <Container>
+
+        <AreaInput>
+            <Input
+                placeholder="Seu email"
+                value={email}
+                onChangeText={ (text) => setEmail(text) }
+            />
+        </AreaInput>
+
+        <AreaInput>
+            <Input
+                placeholder="Sua senha"
+                value={password}
+                onChangeText={ (text) => setPassword(text) }
+                secureTextEntry={true}
+            />
+        </AreaInput>
+
+        <SubmitButton onPress={() => navigation.navigate('SignIn')} >
+            <SubmitText>Cadastrar</SubmitText>  
+        </SubmitButton>
+          
+      </Container>
+    </Background>
+  );
 }
