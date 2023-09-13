4c670ab9f332a4bb03fca85e9b86751f935b792f
Matheus Maschio <matheuslmaschi0@hotmail.com>
2023-09-13 17:10:19 -0300

    fix: appRoutes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import {BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import PlantDetailScreen from "../screens/PlantDetailScreen";
 import TabNavigator from "./TabNavigator";
 import ProfileScreen from "../screens/ProfileScreen";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { ButtonFavorites } from "../Styles/detailsScreenStyle";
+import { FavoriteContext } from "../contexts/FavoriteContext";
 
 const Stack = createNativeStackNavigator();
 
 type TabNavigation = {
-    Details: undefined;
+    Details: {};
     Perfil: undefined;
     Tab: undefined;
 };
@@ -17,11 +20,31 @@ type TabNavigation = {
 export type TabTypes = BottomTabNavigationProp<TabNavigation, "Details"| "Tab" | "Perfil" >;
 
 function AppRoutes() {
+    const { addItemFavorite, removeFavorite, isFavorite } = useContext(FavoriteContext);
+
     return (
         <Stack.Navigator >
-            <Stack.Screen name= "Tab" component={TabNavigator} options={{ headerShown: false }}/>
-            <Stack.Screen name="Details" component={PlantDetailScreen} options={{headerTitleAlign: 'center'}}/>
-            <Stack.Screen name="Perfil" component={ProfileScreen} />
+            <Stack.Screen   
+                name= "Tab" 
+                component={TabNavigator} 
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen 
+                name="Details" 
+                component={PlantDetailScreen} 
+                options={
+                    {
+                        headerTitleAlign: 'center', 
+
+                    }
+                } 
+            />
+
+            <Stack.Screen 
+                name="Perfil" 
+                component={ProfileScreen}
+            />
         </Stack.Navigator>
     );
 }
